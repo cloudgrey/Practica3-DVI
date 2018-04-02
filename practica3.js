dbe2864bc88870134aedc045efda6ca8b4fbb309
César Godino <cesargr5@gmail.com>
2018-04-02 18:41:47 +0200

    punto 3.9 más o menos hecho, se puede mejorar y ampliar

--- practica3.js
+++ practica3.js
@@ -29,6 +29,14 @@ var game = function() {
 	  RETURN: "intro"
 	});*/
 
+	var pantallaComienzo = true; //es un poco ñapa pero de momento sirve
+		/*
+		si no hago eso del true, en cualquier momento de la partida
+		cuando pulse enter (el confirm) se reinicia,
+		por eso asi controlo que solo haga efecto en la pantalla de inicio
+		*/
+
+
 	//-----------------------Se definen las entidades------------------------------------------------
 
 	// ## Player Sprite
@@ -58,21 +66,29 @@ var game = function() {
 				y consulta (sin necesidad de usar getters y setters).
 			*/ 
 			this._super(p, {
-								sheet: "marioR", // Setting a sprite sheet sets sprite width and height 
-								x: 20, // You can also set additional properties that can
+								sheet: "cosasmario", // Setting a sprite sheet sets sprite width and height 
+								sprite: "cosasmario",
+								x: 55, // You can also set additional properties that can
 								y: 528 // be overridden on object creation
 						   }
 						); //_super
 
-			this.add('2d, platformerControls');
+			this.add('2d, platformerControls, animation');
 			// Write event handlers to respond hook into behaviors.
 
-			this.step = function (dt){
+			//Q.input.on("left");
+
+			/*this.step = function (dt){
 				//console.log(this.p);
 				//this.p["x"] = 20;
 				//this.p["y"] = 20;
 				//this.p["z"] = 20;
 				//console.log(this.p["frame"]);
+
+				console.log(Q.input);
+				if(Q.input['right']){
+					this.play("run_right");
+				}
 
 				if(this.p["y"] > 650){
 					//this.p["x"] = 20;
@@ -81,40 +97,74 @@ var game = function() {
 					Q.stageScene("endGame",1, { label: "You Died" }); 
 					this.destroy();
 				}
+			}*/
+			//qué diferencia hay con el step aqui dentro que fuera?
+
+		},//init 
+
+		/*left: function() {
+		    // Play the fire animation at a higher priority
+		    if(this.p.direction > 0) {
+		      this.play("fire_right",1);
+		    } else {
+		      this.play("fire_left",1);
+		    }
+		    console.log("ciao!");
+		},*/
+
+		step: function(dt) {
+			//console.log(Q.input);
+			//console.log(this.p.direction);
+			if(Q.inputs['right']){
+				this.play("run_right");
+			}
+			else if(this.p.direction == "right") {
+				this.play("still_right");
+			}
+			
+			if(Q.inputs['left']){
+				this.play("run_left");
+			}
+			else if(this.p.direction == "left") {
+				this.play("still_left");
 			}
 
-		}//init 
+			if(Q.inputs['up']){
+				if(this.p.direction == "right") {
+					this.play("jump_right");
+				}
+				else if(this.p.direction == "left") {
+					this.play("jump_left");
+				}
+			}
+
+			if(this.p["y"] > 650){
+				//this.p["x"] = 20;
+				//this.p["y"] = 528;
+				console.log("Tas caío lol");
+				Q.stageScene("endGame",1, { label: "You Died" }); 
+				this.destroy();
+			}
+		}//step
 
 	});//extend Mario
 
 	Q.Sprite.extend("Goomba",{
 
-		// the init constructor is called on creation
-		//Se puede redefinir un método definido en una superclase, 
-		//y es posible invocar la versión redefinida con this._super(..).
 		init: function(p) {
 
 			console.log("Estamos creando una instancia de Goomba");
 
-	    	// You can call the parent's constructor with this._super(..)
-			/*
-				La constructora padre (de Sprite) es: init(p,defaults).
-				Recibe un objeto con los atributos a asignar al sprite que se está creando 
-				y los mezcla con las propiedades de otro objeto que contiene valores por defecto. 
-				Los datos de un sprite se guardan en el atributo p del sprite. 
-				Estos atributos son públicos para hacer más eficiente su modificación 
-				y consulta (sin necesidad de usar getters y setters).
-			*/ 
 			this._super(p, {
 								sheet: "goomba", // Setting a sprite sheet sets sprite width and height 
+								sprite: "goomba", //agrego sprite por la animacion, sino solo hace falta sheet
 								x: 1478, // You can also set additional properties that can
 								y: 494, // be overridden on object creation
 								vx: 100
 						   }
 						); //_super
 
-			this.add('2d, aiBounce');//, platformerControls');
-			// Write event handlers to respond hook into behaviors.
+			this.add('2d, aiBounce, animation');
 
 			this.on("bump.left,bump.right",function(collision) {
 
@@ -138,19 +188,7 @@ var game = function() {
 			});//on bump top 
 
 			this.step = function (dt){
-				//console.log(this.p);
-				//this.p["x"] = 20;
-				//this.p["y"] = 20;
-				//this.p["z"] = 20;
-				//console.log(this.p["frame"]);
-
-				if(this.p["y"] > 650){
-					this.p["x"] = 60;
-					this.p["y"] = 528;
-					console.log("Sa caío lol");
-					//Q.stageScene("endGame",1, { label: "You Died" }); 
-					//this.destroy();
-				}
+				this.play("normal");
 			}
 
 		}//init 
@@ -159,24 +197,13 @@ var game = function() {
 
 	Q.Sprite.extend("Bloopa",{
 
-		// the init constructor is called on creation
-		//Se puede redefinir un método definido en una superclase, 
-		//y es posible invocar la versión redefinida con this._super(..).
 		init: function(p) {
 
 			console.log("Estamos creando una instancia de Bloopa");
 
-	    	// You can call the parent's constructor with this._super(..)
-			/*
-				La constructora padre (de Sprite) es: init(p,defaults).
-				Recibe un objeto con los atributos a asignar al sprite que se está creando 
-				y los mezcla con las propiedades de otro objeto que contiene valores por defecto. 
-				Los datos de un sprite se guardan en el atributo p del sprite. 
-				Estos atributos son públicos para hacer más eficiente su modificación 
-				y consulta (sin necesidad de usar getters y setters).
-			*/ 
 			this._super(p, {
 								sheet: "bloopa", // Setting a sprite sheet sets sprite width and height 
+								sprite: "bloopa",
 								x: 200, // You can also set additional properties that can
 								y: 527 // be overridden on object creation
 								//vy: -60,
@@ -184,8 +211,9 @@ var game = function() {
 						   }
 						); //_super
 
-			this.add('2d');//, aiBounce');//, platformerControls');
-			// Write event handlers to respond hook into behaviors.
+			this.add('2d, animation');
+
+			this.on("died",this,"muerto");
 
 			this.on("bump.left,bump.right,bump.bottom",function(collision) {
 
@@ -201,19 +229,15 @@ var game = function() {
 			this.on("bump.top",function(collision) {
 
 				if(collision.obj.isA("Mario")) {
-					this.destroy();
-				    collision.obj.p.vy = -300;
+					this.play("muere");
 				    //Q.stageScene("endGame",1, { label: "You Won!" });
 				}
 
 			});//on bump top 
 
 			this.step = function (dt){
-				//console.log(this.p);
-				//this.p["x"] = 20;
-				//this.p["y"] = 20;
-				//this.p["z"] = 20;
-				//console.log(this.p["frame"]);
+
+				this.play("normal");
 
 				if(this.p["y"] < 528-50){//en realidad no hace falta este if, siempre entra en else
 					this.p["gravity"] = 1;
@@ -231,28 +255,20 @@ var game = function() {
 				}
 			}
 
-		}//init 
-
+		},//init 
+		muerto: function() {
+			console.log("hey");
+		    //this.destroy();
+			//collision.obj.p.vy = -300;
+		},
 	});//extend Bloopa
 
 	Q.Sprite.extend("Princess",{
 
-		// the init constructor is called on creation
-		//Se puede redefinir un método definido en una superclase, 
-		//y es posible invocar la versión redefinida con this._super(..).
 		init: function(p) {
 
 			console.log("Estamos creando una instancia de Princess");
 
-	    	// You can call the parent's constructor with this._super(..)
-			/*
-				La constructora padre (de Sprite) es: init(p,defaults).
-				Recibe un objeto con los atributos a asignar al sprite que se está creando 
-				y los mezcla con las propiedades de otro objeto que contiene valores por defecto. 
-				Los datos de un sprite se guardan en el atributo p del sprite. 
-				Estos atributos son públicos para hacer más eficiente su modificación 
-				y consulta (sin necesidad de usar getters y setters).
-			*/ 
 			this._super(p, {
 								asset: "princess.png", // Setting a sprite sheet sets sprite width and height 
 								x: 1956, // You can also set additional properties that can
@@ -261,15 +277,9 @@ var game = function() {
 						); //_super
 
 			this.add('2d');
-			// Write event handlers to respond hook into behaviors.
-
 			this.on("bump.top,bump.left,bump.right,bump.bottom",function(collision) {
-
-				if(collision.obj.isA("Mario")) {
-				    Q.stageScene("endGame",1, { label: "You Won!" });
-				}
-
-			});
+				if(collision.obj.isA("Mario")) Q.stageScene("endGame",1, { label: "You Won!" });
+			});//on
 
 		}//init 
 
@@ -296,8 +306,9 @@ var game = function() {
 
 		// Create the player and add them to the stage
 		var player = stage.insert(new Q.Mario());
+		//console.log(player);
 		stage.add("viewport").follow(player);//, { x: true, y: false });
-		stage.viewport.offsetX = -140;
+		stage.viewport.offsetX = -110;
 		stage.viewport.offsetY = 160;
 		//stage.centerOn(150, 380);
 
@@ -351,6 +362,8 @@ var game = function() {
 
 	Q.scene('startGame',function(stage) {
 
+		pantallaComienzo = true;
+
 		var container = stage.insert(new Q.UI.Container({
 															x: Q.width/2, 
 															y: Q.height/2, 
@@ -365,6 +378,7 @@ var game = function() {
 		button.on("click",function() {
 		  //Q.clearStages();
 		  Q.stageScene('level1');
+		  pantallaComienzo = false;
 		});
 
 		/*Q.input.on("confirm",this,function() {
@@ -372,12 +386,6 @@ var game = function() {
 		    Q.stageScene('level1');
 		});*/
 
-		var pantallaComienzo = true; //es un poco ñapa pero de momento sirve
-		/*
-		si no hago eso del true, en cualquier momento de la partida
-		cuando pulse enter (el confirm) se reinicia,
-		por eso asi controlo que solo haga efecto en la pantalla de inicio
-		*/
 		Q.input.on("confirm",this,function() {
 		    // Do something
 		    if(pantallaComienzo) Q.stageScene('level1');
@@ -414,12 +422,39 @@ var game = function() {
 	});*/
 
 	Q.loadTMX("level.tmx, mario_small.png, mario_small.json, goomba.png, goomba.json, bloopa.png, bloopa.json, princess.png, mainTitle.png", function() {
-	  Q.stageScene("startGame");
+	  
 	  Q.compileSheets("mario_small.png", "mario_small.json");
 	  Q.compileSheets("goomba.png", "goomba.json");
 	  Q.compileSheets("bloopa.png", "bloopa.json");
 	  Q.compileSheets("princess.png");
 	  //Q.stageScene("level1");
+
+	  Q.animations("cosasmario", {
+		  run_right: { frames: [1,2,3], rate: 1.5/15},
+		  run_left: { frames: [15,16,17], rate: 1.5/15},
+		  still_right: { frames: [0], rate: 1/15},
+		  still_left: { frames: [14], rate: 1/15},
+		  jump_right: { frames: [4], rate: 1/15},
+		  jump_left: { frames: [18], rate: 1/15}
+	  });
+
+	  Q.animations("bloopa", {
+		  normal: { frames: [0,1], rate: 8/15},
+		  muere: { frames: [2], rate: 1/15, trigger: "died"}
+	  });
+
+	  Q.animations("goomba", {
+		  normal: { frames: [0,1], rate: 8/15},
+		  muere: { frames: [2,3], rate: 8/15}
+	  });
+
+	  Q.stageScene("startGame");
+	  /*"marioR":{"sx":0,"sy":0,"tileW":31,"tileH":32,"frames":3},
+		"marioL":{"sx":448,"sy":0,"tileW":32,"tileH":32,"frames":3},
+		"marioDie":{"sx":384,"sy":0,"tileW":32,"tileH":32,"frames":1},
+		"marioJump":{"sx":128,"sy":0,"tileW":32,"tileH":32,"frames":1}
+	   */
+
 	});
 
 	/*Q.loadTMX("level.tmx, goomba.png, goomba.json", function() {
